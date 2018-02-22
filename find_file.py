11e10c41a4e28ccaf2a6b6d70a0de71d6be79734
= <=>
2018-02-22 17:32:18 -0500

    updated find_file in order to allow us to load the oldest file in a directory.

--- find_file.py
+++ find_file.py
@@ -31,7 +31,7 @@ def find_most_recent_file(search_path,pattern = None):
         raise TypeError("<pattern> must be a string argument!"); 
     if not os.path.isdir(search_path): raise Exception("input must be a path!");
     if not os.path.isabs(search_path):
-        print("Warning: you may encounter errors if the path provided isn't local, or complete.\n\%sn"%(search_path))
+        print("Warning: you may encounter errors if the path provided isn't local, or complete.\n\n%s"%(search_path))
     path_items = []
     for a in os.listdir(search_path):
         if os.path.isabs(search_path):
@@ -46,3 +46,32 @@ def find_most_recent_file(search_path,pattern = None):
         path_items.append(tuple(local_item))
     most_recent = max_tup(path_items,1)
     return most_recent;
+    
+def find_least_recent_file(search_path,pattern = None):
+    """
+    will attempt to find the least recently modified file in search_path,
+    matching pattern "pattern". I'll prabably adjust this to do a regex search
+    later, but for now, only capable of a rudimentary containment search.
+    """
+    pattern = pattern or "";
+    if type(search_path) != str:
+        log("TypeError occurred: [search_path] must be a string argument.","error_log.log");
+        raise TypeError("[search_path] must be a string argument!");
+    if type(pattern) != str:
+        log("TypeError occurred: <pattern> must be a string argument.","error_log.log");
+        raise TypeError("<pattern> must be a string argument!");
+    if not os.path.isdir(search_path): raise Exception("input must be a path!");
+    if not os.path.isabs(search_path):
+        print("Warning: you may encounter errors if the pat provided isn't local, or complete.\n\n%s"%(search_path))
+    path_items = [];
+    for a in os.listdir(search_path):
+        if os.path.isabs(search_path):
+            a = search_path+os.sep+a;
+       local_item = []
+       print(a);
+       if pattern not in a: continue;
+       local_item.append(a); local_item.append(os.path.getmtime(a));
+       log("Adding items to consider: %s"%(str(local_item)));
+       path_items.append(tuple(local_item))
+    least_recent = min_tup(path_items,1);
+    return least_recent;
